c43286d228762b001edb24b3c2dc110a590f8f52
Felipe <fas.felipesantos@gmail.com>
2023-12-07 10:41:03 -0300

    ui interface done

---
+++ main.js
@@ -0,0 +1,66 @@
+// Modules
+const { app, BrowserWindow } = require("electron");
+const windowStateKeeper = require("electron-window-state");
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+// Create a new BrowserWindow when `app` is ready
+function createWindow() {
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 500,
+    defaultHeight: 650,
+  });
+
+  mainWindow = new BrowserWindow({
+    x: mainWindowState.x,
+    y: mainWindowState.y,
+    width: mainWindowState.width,
+    height: mainWindowState.height,
+    minWidth: 350,
+    maxWidth: 650,
+    minHeight: 300,
+    // frame: false,
+    webPreferences: {
+      // --- !! IMPORTANT !! ---
+      // Disable 'contextIsolation' to allow 'nodeIntegration'
+      // 'contextIsolation' defaults to "true" as from Electron v12
+      // contextIsolation: false,
+      nodeIntegration: true,
+    },
+    // show: false
+  });
+
+  // Load index.html into the new BrowserWindow
+  mainWindow.loadFile("renderer/main.html");
+  // mainWindow.loadURL('https://watch.clickservices.com.br')
+
+  mainWindowState.manage(mainWindow);
+
+  // Open DevTools - Remove for PRODUCTION!
+  // mainWindow.webContents.openDevTools();
+
+  // Show window when page is ready
+  // mainWindow.on('ready-to-show', () => {
+  //   mainWindow.show()
+  // })
+
+  // Listen for window being closed
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+// Electron `app` is ready
+app.on("ready", createWindow);
+
+// Quit when all windows are closed - (Not macOS - Darwin)
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+// When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
+app.on("activate", () => {
+  if (mainWindow === null) createWindow();
+});
